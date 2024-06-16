1f1edccbd32b6a4587183aed75a5bbc9c050b1fb
MathiasIT22 <mathias.salla@rak.ee>
2024-06-16 21:04:57 +0300

    Update AudioTest.ps1
    Parandasin # ära.

--- AudioTest.ps1
+++ AudioTest.ps1
@@ -1,7 +1,7 @@
 try {
 	[System.Console]::Beep(500,300)
 	Start-Sleep -milliseconds 300
-	exit 0 #vau
+	exit 0 #Teeb arvutisse lühikese heli ja ootab veidi aega enne kui lõpetab.
 } catch {
 	"Error $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
 	exit 1
